7f04c4c26a5c5041f23012e23b9ce178b1fc1294
MikeB91 <mike_b91@hotmail.com>
2021-04-26 16:29:16 -0500

    PERMISSION BASED AUTHENTICATION

--- src/main/java/com/springsecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/com/springsecurity/security/ApplicationSecurityConfig.java
@@ -3,6 +3,7 @@ package com.springsecurity.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -13,6 +14,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 import static com.springsecurity.security.ApplicationUserRole.*;
+import static com.springsecurity.security.ApplicationUserPermission.*;
 
 @Configuration
 @EnableWebSecurity
@@ -28,9 +30,14 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter{
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
+		.csrf().disable() // TODO: I WILL LEARN THIS IN THE NEXT SECTION
 		.authorizeRequests()  //queremos autorizar peticiones
 		.antMatchers("/", "index", "/css/*", "/js/*").permitAll()
 		.antMatchers("/api/**").hasRole(STUDENT.name()) //Protects the end-point with the specified role
+		.antMatchers(HttpMethod.DELETE, "management/api/**").hasAuthority(COURSE_WRITE.name())
+		.antMatchers(HttpMethod.POST, "management/api/**").hasAuthority(COURSE_WRITE.name())  //PERMISSION BASED AUTH 
+		.antMatchers(HttpMethod.PUT, "management/api/**").hasAuthority(COURSE_WRITE.name())   // In this case, the app verifies if the 
+		.antMatchers(HttpMethod.GET, "management/api/**").hasAnyRole(ADMIN.name(), ADMINTRAINEE.name())//user has the correct permission
 		.anyRequest() //Cualquier request del API
 		.authenticated() // Debe estar autenticado
 		.and()
@@ -52,11 +59,17 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter{
 			.roles(ADMIN.name())
 			.build();
 		
-		//This is ROLE BASED AUTHENTICATION
+		UserDetails tomUser = User.builder()
+				.username("tom")
+				.password(passwordEncoder.encode("password123"))
+				.roles(ADMINTRAINEE.name()) //ADMINTRAINEE
+				.build();
+		
 		
 		return new InMemoryUserDetailsManager(
 			annaSmithUser,
-			lindaUser
+			lindaUser,
+			tomUser
 		);
 				
 	}
